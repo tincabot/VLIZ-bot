09fc8020fd2f9ae727b9f99dd6a4eadfd6b0e68b
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-29 08:43:06 -0500

    no se que hago

---
+++ plugins/_auto-aceptar.js
@@ -0,0 +1,21 @@
+// Código Tomado De David-Chian
+
+let handler = m => m
+
+handler.before = async function (m, {conn, isAdmin, isBotAdmin}) {
+if (!m.isGroup) return !1
+let chat = global.db.data.chats[m.chat]
+if (chat.autoAceptar && !isAdmin) {
+    if (!isBotAdmin) return !0
+        const participants = await conn.groupRequestParticipantsList(m.chat)
+        const latinPrefix = '51'
+        const filteredParticipants = participants.filter(p => p.jid.includes('@s.whatsapp.net') && p.jid.split('@')[0].startsWith(latinPrefix))
+        for (const participant of filteredParticipants) {
+            await conn.groupRequestParticipantsUpdate(m.chat, [participant.jid], "approve")
+        }
+        if (m.messageStubType === 172 && m.messageStubParameters) {
+            const [jid] = m.messageStubParameters
+            if (jid.includes('@s.whatsapp.net') && jid.split('@')[0].startsWith(latinPrefix)) {
+                await conn.groupRequestParticipantsUpdate(m.chat, [jid], "approve")}}
+}}
+export default handler
